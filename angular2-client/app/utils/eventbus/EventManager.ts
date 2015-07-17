f72030e2650071faa1b8bb362050d593d70bed6a
Tjerk Valentijn <tjerk.valentijn@inology.nl>
2015-07-17 12:35:09 +0200

    rename MessageInterface to Message

--- angular2-client/app/utils/eventbus/EventManager.ts
+++ angular2-client/app/utils/eventbus/EventManager.ts
@@ -3,7 +3,7 @@
  * Based on: http://csharperimage.jeremylikness.com/2012/11/building-javascript-event-aggregator.html
  */
 
-import {MessageInterface} from "utils/eventbus/MessageInterface";
+import {Message} from "utils/eventbus/MessageInterface";
 import {MessageImpl} from "utils/eventbus/MessageImpl";
 import {Subscription} from "utils/eventbus/Subscription";
 
@@ -29,20 +29,20 @@ export class EventManager {
     }
 
     subscribe(message: string, callback: (payload?: any) => void ) {
-        var msg: MessageInterface;
-        msg = this._messages[message] || <MessageInterface>(this._messages[message] = new MessageImpl(message));
+        var msg: Message;
+        msg = this._messages[message] || <Message>(this._messages[message] = new MessageImpl(message));
         return msg.subscribe(callback);
     }
 
     unSubscribe(message: string, token: number) {
         if (this._messages[message]) {
-            (<MessageInterface>(this._messages[message])).unSubscribe(token);
+            (<Message>(this._messages[message])).unSubscribe(token);
         }
     }
 
     publish(message: string, payload?: any) {
         if (this._messages[message]) {
-            (<MessageInterface>(this._messages[message])).notify(payload);
+            (<Message>(this._messages[message])).notify(payload);
         }
     }
 }
