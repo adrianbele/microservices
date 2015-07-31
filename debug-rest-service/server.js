c9cdb8973ee2314e8d7c19d2fc6627e38cd8b8d2
Tjerk Valentijn <tjerk.valentijn@inology.nl>
2015-07-31 09:57:24 +0200

    nodejs debug server is now feature complete

--- debug-rest-service/server.js
+++ debug-rest-service/server.js
@@ -14,24 +14,22 @@ server.all('/*', function(req, res, next) {
     next();
 });
 
-server.post("/api/newToken", function(req, res) {
-	Item.find(function (err, items) {
-		if (err) {
-			console.log(err);
-		}
-
-		// TODO make proper token
-		var token = "dkjhgdkighdfg";
-
-		res.setHeader("Content-Type", "text/plain");
-        res.end(JSON.stringify(token));
-    });
+server.get("/api/newToken", function(req, res) {
+	var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ";
+    res.setHeader("Content-Type", "text/plain");
+    res.end(JSON.stringify(token));
 });
 
 server.get("/api/tasks/tim", function(req, res) {
-	var tasks = [{"description":"Task description","_id":"1","assignee":"Tim","title":"Report an issue","user_id":"1"}];
+	var tasks = {"actionResult":[{"description":"Task description","_id":"1","assignee":"Tim","title":"Report an issue","user_id":"1"}]};
 	res.setHeader("Content-Type", "application/json");
     return res.send(tasks);
+});
+
+server.post("/api/tasks/tim", function(req, res) {
+    console.log(req.body);
+    var obj = {"actionResult":{"_id": "nodejs" + Math.floor(Math.random() * 111111)}};
+    return res.send(obj);
 });
 
 server.post("/api/log", jsonParser, function(req, res) {
